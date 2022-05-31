802dbe1e73cc21b223d39d31ce321d4ae28ed1b4
pandroza <60747184+pandroza@users.noreply.github.com>
2022-05-31 09:09:38 +0100

    aoc1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+const $ = window.$;
+//import 'css-doodle';
+
+function App() {
+  return (
+    <css-doodle>{`
+      :doodle {
+        @grid: 2 / 200px;
+        grid: 1px;
+      }
+      background: @pick(red, pink);
+    `}</css-doodle>
+  );
+}
+
+export default App;
