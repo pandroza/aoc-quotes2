6537dc8cc35853b800010b45ed88975281d4a592
pandroza <60747184+pandroza@users.noreply.github.com>
2022-06-07 08:49:31 +0100

    n2

--- src/App.js
+++ src/App.js
@@ -90,11 +90,7 @@ class Box1 extends React.Component {
         </div>
       <div className="caro">
         <div id="carodemo" className="carousel slide" data-bs-ride="carousel">
-        <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carodemo" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carodemo" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carodemo" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
+        
   <div className="carousel-inner">
     <div className="carousel-item active">
       <svg
